d86a96ce06799a8d328583a51a91469127e787e0
farhande110 <farhansadeek019@gmail.com>
2022-09-14 10:02:05 -0400

    Chef and Feedback CodeChef

---
+++ Chef_and_Feedback.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string a; cin >> a;
+    for(int i = 0; i < a.length() - 2; i++){
+        if((a[i] == '0' && a[i + 1] == '1' && a[i + 2] == '0') || (a[i] == '1' && a[i + 1] == '0' && a[i + 2] == '1')){
+            cout << "Good\n"; 
+            return;
+        }
+    }
+    cout << "Bad\n";
+}
+
+int main(){
+    int t; cin >> t;
+    for(int i = 0; i < t; i++){
+        solve();
+    }
+    return 0;
+}
