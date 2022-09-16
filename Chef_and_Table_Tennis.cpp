62e322fbd38677bc5deae7ddff5e4e0713cd90ae
farhande110 <farhansadeek019@gmail.com>
2022-09-16 00:24:42 -0400

    Chef and Table Tennis

---
+++ Chef_and_Table_Tennis.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string s; cin >> s;
+    int p = 0, c = 0;
+    if(s[s.length() - 1] == '1') cout << "WIN\n";
+    else cout << "LOSE\n";
+}
+
+int main(){
+    int t; cin >> t;
+    for(int i = 0; i < t; i++){
+        solve();
+    }
+    return 0;
+}
