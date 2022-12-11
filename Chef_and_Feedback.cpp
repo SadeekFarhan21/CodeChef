0a606b0f8b1a6b805a4fd20aafa9f734fa579e27
SadeekFarhan21 <farhansadeek019@gmail.com>
2022-12-11 17:53:15 -0500

    Chef and Feedback C++

--- Chef_and_Feedback.cpp
+++ Chef_and_Feedback.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 void solve(){
     string a; cin >> a;
-    for(int i = 0; i < a.length() - 2; i++){
+    for(int i = 0; i < a.length() - 1; i++){
         if((a[i] == '0' && a[i + 1] == '1' && a[i + 2] == '0') || (a[i] == '1' && a[i + 1] == '0' && a[i + 2] == '1')){
             cout << "Good\n"; 
             return;
         }
     }
     cout << "Bad\n";
+    return;
 }
 
 int main(){
