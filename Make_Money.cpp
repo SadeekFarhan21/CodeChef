95df208e2d491548a3706ce67b8eef7878725b9d
SadeekFarhan21 <farhansadeek019@gmail.com>
2022-12-10 22:10:59 -0500

    Make Money C++

---
+++ Make_Money.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, x, c; cin >> n >> x >> c;
+    int cost = 0;
+    vector <int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for(int i = 0; i < x / c; i++){
+        if((x - v[i]) > c){
+            v[i] = x;
+            cost += c;
+        }
+    }
+    cout << accumulate(v.begin(), v.end(), 0) - cost << "\n";
+}
+
+int main(){
+    int t; cin >> t;
+    for(int i = 0; i < t; i++){
+        solve();
+    }
+}
