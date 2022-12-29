9a011f6aa8d95280a80336d622c00e91fdc3578f
SadeekFarhan21 <farhansadeek019@gmail.com>
2022-12-28 19:47:42 -0500

    Pet Store

--- Pet_Store.cpp
+++ Pet_Store.cpp
@@ -4,10 +4,19 @@ using namespace std;
 void solve(){
     int n; cin >> n;
     vector <int> v(n);
+    map <int, int> m;
     for(int i = 0; i < n; i++){
         cin >> v[i];
+        m[v[i]]++;
     }
-    
+    for(auto x : m){
+        if(x.second % 2 != 0){
+            cout << "NO" << "\n";
+            return;
+        }
+    }
+    cout << "YES" << "\n";
+    return;
 }
 
 int main(){
@@ -15,5 +24,4 @@ int main(){
     for(int i = 0; i < t; i++){
         solve();
     }
-    return 0;
 }
